d84137fc6358fb8e2276d2d6c4e3e3538090e0c0
Ezequias Silva <ezequiasmoises@gmail.com>
2019-06-10 09:31:52 -0300

    initiating the development of adjacency matrix for graphs

---
+++ MDA_Grafo.h
@@ -0,0 +1,19 @@
+/**
+ * @file MDA_Grafo.h
+ * @author Ezequias Silva (ezequiasmoises@gmail.com)
+ * @brief Matriz de adjacencias para grafos(adjacency matrix for graphs)
+ * @version 0.1
+ * @date 2019-06-10
+ * 
+ * @copyright Copyright (c) 2019
+ */
+
+#ifndef MDA_GRAFO_H
+#define MDA_GRAFO_H
+struct MDA_Grafo{
+
+};
+
+typedef MDA_Grafo MDA_Grafo;
+
+#endif
